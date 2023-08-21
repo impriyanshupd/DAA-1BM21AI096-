82e204bbcbadf2d92f8ae5150550ab2541a52478
Priyanshu Prasad <139117337+impriyanshupd@users.noreply.github.com>
2023-08-22 00:42:25 +0530

    Add files via upload

---
+++ Topological Sorting(Source Removal).cpp
@@ -0,0 +1,98 @@
+ #include <bits/stdc++.h>
+using namespace std;
+
+
+class Graph {
+	int V;
+	list<int>* adj;
+
+public:
+	Graph(int V);
+
+	void addEdge(int u, int v);
+
+	void topologicalSort();
+};
+
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj = new list<int>[V];
+}
+
+void Graph::addEdge(int u, int v)
+{
+	adj[u].push_back(v);
+}
+
+void Graph::topologicalSort()
+{
+	vector<int> in_degree(V, 0);
+	for (int u = 0; u < V; u++) {
+		list<int>::iterator itr;
+		for (itr = adj[u].begin();
+			itr != adj[u].end(); itr++)
+			in_degree[*itr]++;
+	}
+
+
+	queue<int> q;
+	for (int i = 0; i < V; i++)
+		if (in_degree[i] == 0)
+			q.push(i);
+
+
+	int cnt = 0;
+
+	vector<int> top_order;
+
+	while (!q.empty()) {
+
+		int u = q.front();
+		q.pop();
+		top_order.push_back(u);
+
+		list<int>::iterator itr;
+		for (itr = adj[u].begin();
+			itr != adj[u].end(); itr++)
+
+			if (--in_degree[*itr] == 0)
+				q.push(*itr);
+
+		cnt++;
+	}
+
+	if (cnt != V) {
+		cout << "There exists a cycle in the graph\n";
+		return;
+	}
+
+	for (int i = 0; i < top_order.size(); i++)
+		cout << top_order[i] << " ";
+	cout << endl;
+}
+
+int main()
+{
+    int n,m,a,b;
+    cout<<"enter number of vertices : ";
+    cin>>n;
+    Graph g(n);
+
+    cout<<"enter number of edges : ";
+    cin>>m;
+
+    for(int i=0;i<m;i++)
+    {
+        cout<<"enter 1st edge of edge "<<i<<" : ";
+        cin>>a;
+        cout<<"enter 2nd edge of edge "<<i<<" : ";
+        cin>>b;
+        g.addEdge(a,b);
+    }
+
+	cout << "Following is a Topological Sort of\n";
+	g.topologicalSort();
+
+	return 0;
+}
